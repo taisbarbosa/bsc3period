c117eafa32c16baf515ba0f79a72ca7c04a2e7f6
taisbarbosa <taisbarbosarodrigues2004@gmail.com>
2024-03-15 17:22:40 -0300

    laboratory

---
+++ estrutura_dados/laboratory02/pointer_and_vector/illustration.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 30;
+    int* x = i;
+
+    int v[] = {1,2,3,4};
+    int* p = v[0];
+    
+    int* w = v[2];
+
+    printf("\n%d  %d %d\n", x, p, w); 
+}
+
+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
