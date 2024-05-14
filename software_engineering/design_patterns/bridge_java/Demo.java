99ac470c1ff054a07772241f37c34cda479cbebe
taisbarbosa <taisbarbosarodrigues2004@gmail.com>
2024-05-13 23:12:28 -0300

    design

---
+++ software_engineering/design_patterns/bridge_java/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new TV());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
